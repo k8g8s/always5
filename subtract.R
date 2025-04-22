f108335d6b7f5aad95377831e447eaf82f475129
MacBeing3 <dawsonmac05@gmail.com>
2025-04-22 14:28:46 -0400

    subtract

---
+++ subtract.R
@@ -0,0 +1,9 @@
+#Maggie W and Dawson M
+
+subtract <- function(number,userNumber){
+  
+  result <- number - userNumber
+  return(result)
+}
+
+#Hi, dont eat popsicles when the apopsiclyspe is occuring. They will be mad :D
